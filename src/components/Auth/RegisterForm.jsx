9ddb43f6308fa94947f694e48dae17ec6dded54f
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-11-16 08:01:56 +0700

    feat: integrate registration form with api

--- src/components/Auth/RegisterForm.jsx
+++ src/components/Auth/RegisterForm.jsx
@@ -1,22 +1,52 @@
 import React from 'react';
 
-const RegisterForm = () => {
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const RegisterForm = ({
+  formData,
+  error,
+  onHandleSubmit,
+  onHandleChange,
+  loading,
+  success,
+}) => {
   return (
-    <form action="#" className="space-y-4 md:space-y-6">
+    <form className="space-y-4 md:space-y-6" onSubmit={onHandleSubmit}>
       <div>
         <label
           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          htmlFor="email"
+          htmlFor="name"
         >
-          Your email
+          Your name
         </label>
         <input
+          required
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          id="email"
-          name="email"
-          placeholder="name@company.com"
-          required=""
-          type="email"
+          id="name"
+          name="name"
+          placeholder="John Doe"
+          type="text"
+          value={formData.name}
+          onChange={onHandleChange}
+        />
+      </div>
+      <div>
+        <label
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          htmlFor="username"
+        >
+          Your Username
+        </label>
+        <input
+          required
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          id="username"
+          name="username"
+          placeholder="Username"
+          type="text"
+          value={formData.username}
+          onChange={onHandleChange}
         />
       </div>
       <div>
@@ -33,66 +63,43 @@ const RegisterForm = () => {
           placeholder="••••••••"
           required=""
           type="password"
+          value={formData.password}
+          onChange={onHandleChange}
         />
       </div>
-      <div>
-        <label
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          htmlFor="confirm-password"
-        >
-          Confirm password
-        </label>
-        <input
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          id="confirm-password"
-          name="confirm-password"
-          placeholder="••••••••"
-          required=""
-          type="confirm-password"
-        />
-      </div>
-      <div className="flex items-start">
-        <div className="flex items-center h-5">
-          <input
-            aria-describedby="terms"
-            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-            id="terms"
-            required=""
-            type="checkbox"
-          />
-        </div>
-        <div className="ml-3 text-sm">
-          <label
-            className="font-light text-gray-500 dark:text-gray-300"
-            htmlFor="terms"
-          >
-            I accept the{' '}
-            <a
-              className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-              href="#"
-            >
-              Terms and Conditions
-            </a>
-          </label>
-        </div>
-      </div>
+      {success && <p>{success}</p>}
+      {error && <p>{error}</p>}
+
       <button
         className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
         type="submit"
       >
-        Create an account
+        {loading ? 'Loading...' : 'Create an account'}
       </button>
       <p className="text-sm font-light text-gray-500 dark:text-gray-400">
         Already have an account?{' '}
-        <a
+        <Link
           className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-          href="#"
+          to="/"
         >
           Login here
-        </a>
+        </Link>
       </p>
     </form>
   );
+};
+
+RegisterForm.propTypes = {
+  formData: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired,
+  }).isRequired,
+  error: PropTypes.string,
+  onHandleSubmit: PropTypes.func.isRequired,
+  onHandleChange: PropTypes.func.isRequired,
+  loading: PropTypes.bool.isRequired,
+  success: PropTypes.string,
 };
 
 export default RegisterForm;
--- src/containers/Auth/RegisterContainer.jsx
+++ src/containers/Auth/RegisterContainer.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import RegisterForm from 'components/Auth/RegisterForm';
+import useAuth from 'hooks/useAuth';
 
 const RegisterContainer = () => {
-  return <RegisterForm />;
+  const { register, loading, error } = useAuth();
+  const [formData, setFormData] = useState({
+    name: '',
+    username: '',
+    password: '',
+    role: 'admin',
+  });
+  const [successMessage, setSuccessMessage] = useState('');
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const response = await register(formData);
+    if (response) {
+      setSuccessMessage(response.message);
+      setFormData({ name: '', username: '', password: '', role: 'admin' });
+    }
+  };
+  return (
+    <RegisterForm
+      error={error}
+      formData={formData}
+      loading={loading}
+      success={successMessage}
+      onHandleChange={handleChange}
+      onHandleSubmit={handleSubmit}
+    />
+  );
 };
 
 export default RegisterContainer;
