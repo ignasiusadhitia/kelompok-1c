317446c2f38414c41b61d0f00eb714acd72f8039
Andrean Nurdiana <andreannrdn@gmail.com>
2024-11-16 23:28:15 +0700

    fix(feature/subscribe): subscribe detail

--- src/components/Subscribe/SubscribeListItem.jsx
+++ src/components/Subscribe/SubscribeListItem.jsx
@@ -58,18 +58,16 @@ const SubscribeListItem = () => {
             </div>
           </div>
           <div className="overflow-x-auto">
-            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <table className="w-full text-sm text-center text-gray-500 dark:text-gray-400">
               <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
-                  <th className="px-4 py-3">Email</th>
+                  <th className="px-4 py-3 w-full">Email</th>
                 </tr>
               </thead>
               <tbody>
                 {Subscribes.map((subscribe) => (
                   <tr key={subscribe.id} className="border-b dark:border-gray-700">
-                    <td className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                      {subscribe.name}
-                    </td>
+                    
                     <td className="px-4 py-3">{subscribe.email}</td>
                     <td className="px-4 py-3 flex items-center justify-center">
                       <button
