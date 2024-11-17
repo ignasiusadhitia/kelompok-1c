9dc3f7fa150affe69b22ddc96f3f2f9ca6ea4697
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-11-17 12:21:23 +0700

    (team): team form import

--- src/containers/Team/TeamAddContainer.jsx
+++ src/containers/Team/TeamAddContainer.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import teamService from 'api/services/teamService';
-import TeamForm from 'components/team/TeamForm';
+import TeamForm from 'components/Team/TeamForm';
 
 const TeamAddContainer = () => {
   const [formData, setFormData] = useState({ name: '', title: '' });
