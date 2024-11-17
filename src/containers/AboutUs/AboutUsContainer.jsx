083a817e24efe395d28a0f07812d696fb62a612d
Febrian Andi Nugroho <110412713+febrian-andi@users.noreply.github.com>
2024-11-17 14:54:33 +0700

    Update src/containers/AboutUs/AboutUsContainer.jsx
    Co-authored-by: Ignasius Yuda Adhitia <81973176+ignasiusadhitia@users.noreply.github.com>

--- src/containers/AboutUs/AboutUsContainer.jsx
+++ src/containers/AboutUs/AboutUsContainer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import useFetchData from '../../hooks/useFetchData';
-import AboutUsList from '../../components/AboutUs/AboutUsList';
+import useFetchData from 'hooks/useFetchData';
+import AboutUsList from 'components/AboutUs/AboutUsList';
 
 const Button = ({ data }) => {
   const navigate = useNavigate();
