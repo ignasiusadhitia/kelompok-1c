ae58ad2c39d748dd88725a6fc9cf4c850c31f90e
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-11-17 12:27:49 +0700

    fix(team): add missing TeamList file

--- src/containers/Team/TeamListContainer.jsx
+++ src/containers/Team/TeamListContainer.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 
 import teamService from 'api/services/teamService';
-import TeamList from 'components/team/TeamList';
+
+import TeamList from '../../components/Team/TeamList';
 
 const TeamListContainer = () => {
   const [team, setTeam] = useState([]);
